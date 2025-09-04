cc3872dae87b1b27ac211420fc3a30d249b5b735
aca20dl <david28gomes@gmail.com>
2025-09-04 20:04:01 +0100

    I can now write text and then display it in the loop - pretty fun hehe

--- text-editor/clieditor.py
+++ text-editor/clieditor.py
@@ -1,21 +1,44 @@
 import curses
 from curses import wrapper
+from curses.textpad import Textbox, rectangle
 import random
+
+# class clieditor:
+#     def __init__(self)
+
 
 def main(stdscr):
     stdscr.clear()
+    editwin = curses.newwin(5,30, 2,1)
+    # rectangle(stdscr, 1,0, 1+5+1, 1+30+1)
+    
+    box = Textbox(editwin)
+
+    box.edit()
+
+    message = box.gather()
+
+    stdscr.refresh()
+
+
+
+    # stdscr.refresh()
     
     while KeyboardInterrupt:
+        curses.echo()            # Enable echoing of characters
+
         stdscr.keypad(True)
+
         i = random.randint(1, 100)/100
         j = random.randint(1, 100)/100
-        stdscr.addstr(int(curses.LINES*i), int(curses.COLS/2*j) ,"World Hello")
+        stdscr.addstr(int(curses.LINES*i), int(curses.COLS/2*j) ,message)
         stdscr.getkey()
         stdscr.clear()
     
 
-    stdscr.refresh()
-    stdscr.getch()
+    # stdscr.refresh()
+    # stdscr.getch()
+    print(message)
     
 
 wrapper(main)
